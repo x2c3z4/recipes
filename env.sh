9961d19d9be1e5e1aa1dd05c41bd58f08ce2e650
fengli <lifeng1519@gmail.com>
2014-07-31 10:43:49 +0800

    add tests for reverse-words-in-string

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH=$PATH:$DIR/thirdparty/blade
+export PPROF_PATH=$DIR/thirdparty/perftools/bin/pprof
--- leetcode/reverse-words-in-string.cpp
+++ leetcode/reverse-words-in-string.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <cstdint>
+
+#include "thirdparty/gtest/gtest.h"
 
 using namespace std;
 
@@ -21,21 +24,46 @@ public:
         ++i;
       }
     }
-  }
-  void dumpVector() {
+
+    ostringstream os;
     for (auto it = container.crbegin(); it != container.crend(); ++it) {
-      cout << *it << ' ';
+      os << *it << ' ';
     }
+    s = os.str();
+    s = s.substr(0, s.length() - 1);
   }
 private:
   vector<string> container;
 };
 
-int main() {
-  string test = " I am a hero ";
+class Solution2 {
+public:
+  void reverseWords(string& s) {
+    istringstream is(s);
+    string tmp = "";
+    string out = "";
+    while (is >> tmp) {
+      tmp += ' ';
+      tmp += out;
+      out = tmp;
+    }
+  }
+};
+
+
+
+string test = " I am a hero ";
+string expected = "hero a am I";
+
+TEST(Solution, reverse1) {
   Solution* s = new Solution();
   s->reverseWords(test);
-  s->dumpVector();
+  ASSERT_TRUE(test == expected);
+}
+
+TEST(Solution2, reverse2) {
+  Solution2* s = new Solution2();
+  s->reverseWords(test);
+  ASSERT_TRUE(test == expected);
   delete s;
-  return 0;
 }
